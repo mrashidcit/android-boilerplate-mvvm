23582fe6789071ad9e77565638cbfb005278fff1
ar9t4 <abdulrahman9t4@gmail.com>
2022-06-10 20:34:32 +0500

    add new convenience methods in utils, app bundle configs for base module (apk)

--- app/src/main/java/com/android/boilerplate/aide/utils/FileUtils.kt
+++ app/src/main/java/com/android/boilerplate/aide/utils/FileUtils.kt
@@ -1,8 +1,11 @@
 package com.android.boilerplate.aide.utils
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
 import com.android.boilerplate.R
 import java.io.File
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -14,6 +17,15 @@ object FileUtils {
     private const val FILENAME_FORMAT = "yyyyMMddHHmmssSSS"
     private const val PHOTO_EXTENSION = ".jpg"
     private const val VIDEO_EXTENSION = ".mp4"
+    private val TAG = this::class.java.simpleName
+
+    /**
+     * Checks if the given path is a file or directory
+     */
+    fun isFile(path: String?): Boolean {
+        path ?: return false
+        return File(path).isFile
+    }
 
     /**
      * Generate name for Image File
@@ -40,5 +52,23 @@ object FileUtils {
     /** Helper function used to create a timestamped file */
     fun createFile(baseFolder: File, fileName: String): File {
         return File(baseFolder, fileName)
+    }
+
+    /**
+     * Convert given bitmap into given file
+     */
+    fun convertBitmapToFile(bitmap: Bitmap?, file: File?): File? {
+        bitmap ?: return null
+        file ?: return null
+        return try {
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            file
+        } catch (e: Exception) {
+            Log.e(TAG, "convertBitmapToFile exception: $e")
+            null
+        }
     }
 }
